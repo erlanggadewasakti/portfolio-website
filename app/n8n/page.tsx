03657dc7d64c8202a5b928766efcd59275e6810a
Erlangga Dewa <erlanggadewasakti@gmail.com>
2025-09-18 17:51:00 +0700

    Update redirect URL in N8nPage component to new n8n instance

--- app/n8n/page.tsx
+++ app/n8n/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 
 // redirect to n8n
 export default function N8nPage() {
-  redirect("https://n8n-uvzrw0mjmqpf.n8x.web.id");
+  redirect("https://n8n-rbc36oro5zam.n8x.my.id");
 }
